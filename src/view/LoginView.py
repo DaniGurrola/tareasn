d517ebb9521ed932bea072a026aa8b0e7dee39da
23308060610671@cetis61.edu.mx <23308060610671@cetis61.edu.mx>
2026-05-05 08:10:20 -0500

    hh

--- src/view/LoginView.py
+++ src/view/LoginView.py
@@ -1,27 +1,67 @@
 import flet as ft
+from views.RegistroView import RegistroView
+from datetime import datetime
 
-def LoginView(page, auth_controller):
-    email_input = ft.TextField(label="Correo electrónico", width=350, border_radius=10)
-    pass_input = ft.TextField(label="Contraseña", password=True, can_reveal_password=True, width=350, border_radius=10)
 
+def LoginView(page: ft.Page, auth_controller):
+    
+    def ver_contra():
+        contra.password = not contra.password
+        contra.update()
+        
+    correo=(ft.TextField(label="Correo",autofocus=True, icon=ft.Icons.PERSON ))
+    contra=(ft.TextField(label="Contraseña",suffix=ft.IconButton(icon=ft.Icons.VISIBILITY, on_click=ver_contra) ,password=True, autofocus=True, icon=ft.Icons.PASSWORD))
+    
+    
+    
     def login_click(e):
-        user, msg = auth_controller.login(email_input.value, pass_input.value)
+        if not correo.value or not contra.value:
+            page.show_dialog(ft.SnackBar(ft.Text("Por favor, complete todos los campos")))
+            return
+        
+    
+        user, msg = auth_controller.login(correo.value, contra.value)
+    
         if user:
-            page.session.set("user", user) 
+            page.user_data = user
             page.go("/dashboard")
         else:
-            page.snack_bar = ft.SnackBar(ft.Text(msg))
-            page.snack_bar.open = True
-            page.update()
-
-    return ft.View("/", [
-        ft.AppBar(title=ft.Text("SIGE - Login"), bgcolor=ft.Colors.BLUE_GREY_900, color="white"),
-        ft.Column([
-            ft.Icon(ft.Icons.LOCK_PERSON, size=50, color=ft.colors.BLUE),
-            ft.Text("Acceso al Sistema", size=20, weight="bold"),
-            email_input,
-            pass_input,
-            ft.ElevatedButton("Entrar", on_click=login_click, width=350),
-            ft.TextButton("Crear una cuenta nueva", on_click=lambda _: page.go("/registro"))
-        ], horizontal_alignment=ft.CrossAxisAlignment.CENTER, alignment=ft.MainAxisAlignment.CENTER)
-    ])
+            page.show_dialog(ft.SnackBar(ft.Text(msg)))
+            
+    def olvidado():
+        page.show_dialog(ft.SnackBar(ft.Text("Se a enviado su contraseña al correo")))
+    
+    def registro():
+        page.go("/registro")
+    
+    iniciar=( ft.Button("Iniciar sesion",color=ft.Colors.WHITE ,bgcolor=ft.Colors.BLUE,on_click=login_click))
+    registrarse =( ft.TextButton("¿Quieres registrarte?", on_click=registro))
+    olvidada =( ft.TextButton("¿Olvidaste la contraseña?", on_click=olvidado))
+    
+    return ft.View(
+        route="/",
+        vertical_alignment=ft.MainAxisAlignment.CENTER, 
+        horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+        appbar=ft.AppBar(
+            title=ft.Text("Login"),
+            bgcolor=ft.Colors.BLUE_GREY_900,
+            color="white"
+        ),
+        controls=[
+            ft.Column(
+                [
+                    ft.Icon(ft.Icons.LOCK_PERSON, size=50, color=ft.Colors.BLUE),
+                    ft.Text("Acceso al sistema", size=24, weight="bold"),
+                    correo,
+                    contra,
+                    iniciar,
+                    registrarse,
+                    olvidada
+                ],
+                horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                spacing=20,
+                tight=True 
+            )
+        ]
+    )
+    
